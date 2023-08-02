eb06f65d1fca27159fadff14511630acb954ba18
Chung Tran <bigearsenal@gmail.com>
2023-08-02 12:55:15 +0700

    feat: navigation view

--- Github Actions/ContentView.swift
+++ Github Actions/ContentView.swift
@@ -2,11 +2,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        WorkflowsTriggerView(
-            repositoryOwner: "p2p-org",
-            repositoryName: "p2p-wallet-ios",
-            personalAccessToken: "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"
-        )
+        NavigationView {
+            WorkflowsTriggerView(
+                repositoryOwner: "p2p-org",
+                repositoryName: "p2p-wallet-ios",
+                personalAccessToken: "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"
+            )
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarTitle("p2p-org/p2p-wallet-ios")
+        }
     }
 }
 
